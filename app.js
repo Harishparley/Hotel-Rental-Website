481eb8057a63e58ec45a7e52cdbd35e88a6bcc12
Harish Parley <harishparleyhp@gmail.com>
2025-10-20 23:53:20 +0530

    Add project files

--- app.js
+++ app.js
@@ -1,19 +1,26 @@
+if (process.env.NODE_ENV != "production") {
+  require("dotenv").config();
+}
+console.log(process.env.SECRET);
+
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const Listing = require("./models/listing.js");
 const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
-const wrapAsync = require("./utils/wrapAsync.js");
 const ExpressError = require("./utils/ExpressError.js");
-const {listingSchema} = require("./schema.js");
+const session = require("express-session");
+const flash = require("connect-flash");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const User = require("./models/user.js");
 
+const listingRouter = require("./routes/listing.js");
+const reviewRouter = require("./routes/review.js");
+const userRouter = require("./routes/user.js");
 
-
- 
-
-const MONGO_URL= "mongodb://127.0.0.1:27017/wanderlust";
+const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
 main()
   .then(() => {
@@ -27,99 +34,64 @@ async function main() {
 }
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
-app.use(express.urlencoded({extended : true}));//hamara data jo req ke andr aa rha he use parse kr paye 
+app.use(express.urlencoded({ extended: true })); //hamara data jo req ke andr aa rha he use parse kr paye
 app.use(methodOverride("_method"));
 app.engine("ejs", ejsMate);
 app.use(express.static(path.join(__dirname, "/public")));
 
-app.get("/", (req, res) => {
-  res.send("hii i am roots");
-});
-
-const validateListing=(req, res, next)=>{
- let {error} = listingSchema.validate(req.body);
-if(error){
-  let errMsg = error.details.map((el)=> el.message).join(",");
-  throw new ExpressError(400, errMsg);
-}else{
-  next();
-}
+const sessionOptions = {
+  secret: process.env.SECRET,
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+    httpOnly: true,
+  },
 };
 
-//index Route
-app.get("/listings", wrapAsync( async(req, res)=>{
-  const allListings = await Listing.find({});
-  res.render("./listings/index.ejs", {allListings});
-}));
-
-//new route 
-app.get("/listings/new", (req, res)=>{
-  res.render("./listings/new.ejs");
-})
-
-
-//show route 
-app.get("/listings/:id",wrapAsync( async(req, res)=>{
-  let{id}= req.params;
-  const listing = await Listing.findById(id);
-  res.render("listings/show.ejs", {listing});
-
-}));
-
-//create route
-app.post("/listings",validateListing, wrapAsync(async(req, res, next)=>{
- 
-const newListing = new Listing(req.body.listing);
-  await newListing.save();
-  res.redirect("/listings")
-  })
-);
-
-//edit route
-app.get("/listings/:id/edit", wrapAsync(async(req, res)=>{
-  let{id}= req.params;
-  const listing = await Listing.findById(id);
-  res.render("listings/edit.ejs", {listing});
-  
-}));
-
-//update route 
-app.put("/listings/:id",validateListing, wrapAsync(async (req, res)=>{
-  let{id}= req.params;
-  await Listing.findByIdAndUpdate(id, {...req.body.listing})
-  res.redirect("/listings/${id}");
-}));
-
-//delet route
-app.delete("/listings/:id", wrapAsync(async(req,res)=>{
-  let{id}=req.params;
-  let deletedListing=await Listing.findByIdAndDelete(id);
-  console.log(deletedListing);
-  res.redirect("/listings");
-
-}));
-
-
-// app.get("/tetslisting", async (req, res) => {
-//   let sampleListing = new Listing({
-//     title: "my new Villa",
-//     description: "By the beach or stars",
-//     price: 1100,
-//     location: "Calangute, Goa",
-//     country: "India",
-//   });
-//   await sampleListing.save();
-//   console.log("Sample was saved");
-//   res.send("Successful testing");
+// app.get("/", (req, res) => {
+//   res.send("hii i am roots");
 // });
+
+app.use(session(sessionOptions));
+app.use(flash());
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use((req, res, next) => {
+  res.locals.success = req.flash("success");
+  res.locals.error = req.flash("error");
+  res.locals.currUser = req.user;
+
+  next();
+});
+
+// app.get("/demoUser", async (req, res) => {
+// let fakeUser = new User({
+// email: "student@gmail.com",
+// username: "delta-student",
+// });
+// let registeredUser = await User.register(fakeUser, "helloworld");
+// res.send(registeredUser);
+// });
+
+app.use("/listings", listingRouter);
+app.use("/listings/:id/reviews", reviewRouter);
+app.use("/", userRouter);
 
 // app.all("*", (req,res,next)=>{
 //   next(new ExpressError(404, "Page Not Found!"));
 // });
 
-app.use((err, req, res, next)=>{
-  let {statusCode=500, message="something wend wrong"}=err;
-  res.status(statusCode).render("error.ejs", {message});
+app.use((err, req, res, next) => {
+  let { statusCode = 500, message = "something wend wrong" } = err;
+  res.status(statusCode).render("error.ejs", { message });
   // res.status(statusCode).send(message);
 });
 
